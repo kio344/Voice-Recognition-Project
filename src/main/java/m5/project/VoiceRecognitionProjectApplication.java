16be97f323041e3d3b502a30cbb5738ac35b20e6
MinHoKim <kimm3654@naver.com>
2023-02-19 00:45:38 +0900

    프로젝트 시작 2023.02.19 ~

---
+++ src/main/java/m5/project/VoiceRecognitionProjectApplication.java
@@ -0,0 +1,13 @@
+package m5.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoiceRecognitionProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VoiceRecognitionProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/m5/project/VoiceRecognitionProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package m5.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoiceRecognitionProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
